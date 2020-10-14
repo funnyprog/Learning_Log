075f1d8656f8bb3a1f7bd0e7342e2db7aa60852d
Pavel <zabotin7513@mail.ru>
2020-10-14 19:42:15 +0300

    Test3

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'jjw-mmdc0*b*$2-8vb&zq7=hc10c6$+!1__^*fi9xj*mb_9p)p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
